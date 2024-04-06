4be7f408c0ae162dae61422d915c8b778ab015c6
johnneon <neon.1598@mail.ru>
2024-04-06 19:14:29 +0300

    feat: add comments

--- model_preparation.py
+++ model_preparation.py
@@ -5,10 +5,23 @@ from sklearn.preprocessing import OrdinalEncoder
 
 
 def preparation_data(path):
+    """
+    Prepare the data for ML
+
+    Params:
+        - path (str): path to file with data
+
+    Returns:
+        - data (pd.DataFrame): dataframe with prepared data
+    """
+
+    # Get dataset from file
     df = pd.read_csv(path)
 
+    # Remove useless column
     df.drop(['ID'], axis=1, inplace=True)
 
+    # Divide features for cat/num
     cat_columns = []
     num_columns = []
 
@@ -18,10 +31,12 @@ def preparation_data(path):
         else:
             num_columns += [column_name]
 
+    # Scaling num features
     scaler = StandardScaler()
     scaler.fit(df[num_columns])
     df[num_columns] = scaler.transform(df[num_columns])
 
+    # Encode cat features
     encoder = OrdinalEncoder()
     encoder.fit(df[cat_columns])
     df[cat_columns] = encoder.transform(df[cat_columns])
@@ -30,8 +45,12 @@ def preparation_data(path):
 
 
 if __name__ == '__main__':
+    # Prep train data
     train = preparation_data('./train/train.csv')
+    # Save it
     train.to_csv('./train/train_prepared.csv')
 
+    # Prep train data
     test = preparation_data('./test/test.csv')
+    # Save it
     test.to_csv('./test/test_prepared.csv')
