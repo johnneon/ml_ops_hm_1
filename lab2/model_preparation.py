f13e92ff4fad6f6f4a9301c755daf3580a7a72d0
johnneon <neon.1598@mail.ru>
2024-04-20 18:11:10 +0300

    feat: add lab 2 prep

---
+++ lab2/model_preparation.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+
+from sklearn.linear_model import LinearRegression
+
+import pickle
+
+separator = '===================================='
+
+# Get data from prepared csv
+df = pd.read_csv('train/train_prepared.csv')
+
+# Get features and target
+X, y = df.drop('price', axis=1), df['price']
+
+# Split it out
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+# Crate model
+lr = LinearRegression()
+
+print('Start learning model...')
+
+# Fit
+lr.fit(X_train, y_train)
+
+print('Finished learning')
+
+# Save model
+with open('model.pkl', 'wb') as f:
+    pickle.dump(lr, f)
+
+print('Model has been saved')
+print(separator)
+print(separator)
+
---
+++ lab2/model_preprocessing.py
@@ -0,0 +1,70 @@
+import pandas as pd
+
+from sklearn.preprocessing import PowerTransformer
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OrdinalEncoder
+
+separator = '===================================='
+
+
+def preparation_data(path):
+    """
+    Prepare the data for ML
+
+    Params:
+        - path (str): path to file with data
+
+    Returns:
+        - data (pd.DataFrame): dataframe with prepared data
+    """
+
+    # Get dataset from file
+    df = pd.read_csv(path)
+
+    # Remove useless column
+    df.drop(['ID', 'name', 'symboling'], axis=1, inplace=True)
+
+    # Divide features for cat/num
+    cat_columns = []
+    num_columns = []
+
+    for column_name in df.columns:
+        if df[column_name].dtypes == object:
+            cat_columns += [column_name]
+        else:
+            num_columns += [column_name]
+
+    # Scaling num features
+    scaler = StandardScaler()
+    scaler.fit(df[num_columns])
+    df[num_columns] = scaler.transform(df[num_columns])
+
+    # Power transform num features
+    power = PowerTransformer()
+    power.fit(df[num_columns])
+    df[num_columns] = power.transform(df[num_columns])
+
+    # Encode cat features
+    encoder = OrdinalEncoder()
+    encoder.fit(df[cat_columns])
+    df[cat_columns] = encoder.transform(df[cat_columns])
+
+    return df
+
+
+if __name__ == '__main__':
+    print('Starting preprocessing training data...')
+    # Prep train data
+    train = preparation_data('train/train.csv')
+    # Save it
+    train.to_csv('./train/train_prepared.csv')
+    print('Finished preprocessing training')
+
+    print('Starting preprocessing testing data...')
+    # Prep train data
+    test = preparation_data('test/test.csv')
+    # Save it
+    test.to_csv('./test/test_prepared.csv')
+    print('Finished preprocessing')
+    print(separator)
+    print(separator)
